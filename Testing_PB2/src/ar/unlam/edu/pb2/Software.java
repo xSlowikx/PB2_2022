86a04c806d666466d28136134d79dc3857a23709
xSlowik <aletogu00@gmail.com>
2022-09-06 16:55:58 -0300

    Avances en el proyecto de lector biometrico, falta ingreso de fichaje usuario

--- Testing_PB2/src/ar/unlam/edu/pb2/Software.java
+++ Testing_PB2/src/ar/unlam/edu/pb2/Software.java
@@ -1,8 +1,12 @@
 package ar.unlam.edu.pb2;
 
+import java.util.ArrayList;
+
 public class Software {
 	private String nombreDelDispositivo;
 	private Integer limiteDeUsuariosRegistrables;
+	private ArrayList<Usuario> usuarios;
+	
 	
 	public Software() {
 		// TODO Auto-generated constructor stub
@@ -11,11 +15,30 @@ public class Software {
 	public Software (String nombreDelDispositivo, Integer limiteDeUsuariosRegistrables) {
 		this.nombreDelDispositivo = nombreDelDispositivo;
 		this.limiteDeUsuariosRegistrables = limiteDeUsuariosRegistrables;
+		this.usuarios = new ArrayList<>();
 	}
 
 	
 
 	public void registrarUnUsuario(Integer numeroDeUsuario, String apellido, TipoDeEmpleado puestoLaboral, Double sueldo) {
 		Usuario nuevoUsuario = new Usuario (numeroDeUsuario, apellido, puestoLaboral, sueldo);
+		this.usuarios.add(nuevoUsuario);
+	}
+
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	public void registrarIngresoUsuario(Integer numeroUsuario, Integer dia, Integer mes) {
+		for(Usuario recorridoUsuario: this.usuarios) {
+			if( (recorridoUsuario.getNumeroDeUsuario().equals(numeroUsuario)) 
+					&& ) {
+				System.out.println("El usuario existe, procediendo a continuar el ingreso...");
+			}
+		}
 	}
 }
--- Testing_PB2/src/ar/unlam/edu/pb2/Usuario.java
+++ Testing_PB2/src/ar/unlam/edu/pb2/Usuario.java
@@ -15,5 +15,37 @@ public class Usuario {
 		this.sueldo = sueldo;
 	}
 
+	public Integer getNumeroDeUsuario() {
+		return numeroDeUsuario;
+	}
+
+	public void setNumeroDeUsuario(Integer numeroDeUsuario) {
+		this.numeroDeUsuario = numeroDeUsuario;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(Double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public TipoDeEmpleado getTipoDeEmpleado() {
+		return tipoDeEmpleado;
+	}
+
+	public void setTipoDeEmpleado(TipoDeEmpleado tipoDeEmpleado) {
+		this.tipoDeEmpleado = tipoDeEmpleado;
+	}
+
 	
 }
